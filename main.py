14bc2c91cfcdc61fc02388265a2a709b5ce67f70
Nikita <khegay.nik@gmail.com>
2023-12-27 10:35:44 +0600

    First commit

---
+++ main.py
@@ -0,0 +1,49 @@
+from flask import Flask, request, jsonify
+import jwt
+import datetime
+from functools import wraps
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'
+
+
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = request.headers.get('Authorization')
+
+        if not token:
+            return jsonify({'message': 'Token is missing'}), 401
+
+        try:
+            data = jwt.decode(token, app.config['SECRET_KEY'])
+        except jwt.ExpiredSignatureError:
+            return jsonify({'message': 'Token has expired'}), 401
+        except jwt.InvalidTokenError:
+            return jsonify({'message': 'Invalid token'}), 401
+
+        return f(data, *args, **kwargs)
+
+    return decorated
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    auth = request.authorization
+
+    if auth and auth.password == 'password':
+        token = jwt.encode({'user': auth.username, 'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30)},
+                           app.config['SECRET_KEY'])
+        return jsonify({'token': token.decode('UTF-8')})
+
+    return jsonify({'message': 'Invalid credentials'}), 401
+
+
+@app.route('/protected', methods=['GET'])
+@token_required
+def protected(data):
+    return jsonify({'message': 'This is a protected route', 'user': data['user']})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
